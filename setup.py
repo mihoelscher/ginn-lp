5b4a45a1d10fad5c2aa6b12687468b6f76086e8d
Nisal Ranasinghe <nsranasinghe@student.unimelb.edu.au>
2024-01-10 11:16:51 +1100

    refactor

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
-setup(name='symnn',
+setup(name='ginnlp',
       version='0.1',
       description='sym NN version 1',
       author='Nisal Ranasinghe',
       author_email='nsranasinghe@student.unimelb.edu.au',
-      packages=['symnn'],
+      packages=['ginnlp'],
       zip_safe=False)
